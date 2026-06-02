bdef94014cc620a43fcb7a83d41876ce7545e645
Raqibyousuf <rywani.786@gmail.com>
2026-06-02 12:52:00 +0000

    imageupdated

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function() {
         
         // This is a sample image. Once you upload your 3D render to the assets folder,
         // change this URL to "assets/your-render-name.jpg"
-        "panorama": "https://pannellum.org/images/alma.jpg",
+        "panorama": "assets/mianpic.jpg",
         
         "autoLoad": true,
         "compass": false
