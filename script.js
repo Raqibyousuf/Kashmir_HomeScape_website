05442f8eca5d3dd43941e0668b3cf57f54fc44c8
Raqibyousuf <rywani.786@gmail.com>
2026-06-02 12:55:40 +0000

    imagenameupdate

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function() {
         
         // This is a sample image. Once you upload your 3D render to the assets folder,
         // change this URL to "assets/your-render-name.jpg"
-        "panorama": "assets/mianpic.jpg",
+        "panorama": "assets/mainpic.jpg",
         
         "autoLoad": true,
         "compass": false
