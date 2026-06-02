0d036e437989abdcd9d51e75f9b4b2cbf4317a90
Raqibyousuf <rywani.786@gmail.com>
2026-06-02 10:19:03 +0000

    Initial kashmir HomeScape Webste

---
+++ script.js
@@ -0,0 +1,15 @@
+// Wait for the HTML to fully load before starting the 3D viewer
+document.addEventListener("DOMContentLoaded", function() {
+    
+    pannellum.viewer('panorama-viewer', {
+        "type": "equirectangular",
+        
+        // This is a sample image. Once you upload your 3D render to the assets folder,
+        // change this URL to "assets/your-render-name.jpg"
+        "panorama": "https://pannellum.org/images/alma.jpg",
+        
+        "autoLoad": true,
+        "compass": false
+    });
+
+});
